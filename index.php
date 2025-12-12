8f006b71189d8632e2366516952c4838b347afb0
furyafes <yafesgorkem@gmail.com>
2025-12-12 22:40:31 +0300

    Ders1

--- index.php
+++ index.php
@@ -1,18 +1,2 @@
 <?php
 echo "Hello World!<br>";
-
-$isim = "Gorkem";
-$yas = 20;
-
-echo "İsim: $isim<br>";
-echo "Yaş: $yas<br>";
-
-if ($yas >= 18) {
-    echo "Ehliyet alabilirsin.<br>";
-} else {
-    echo "Otobus kullanabilirsin.<br>";
-}
-echo "<br>";
-for ($i = 1; $i <= 10; $i++) {
-    echo "$i. Adım<br><br>";
-}
